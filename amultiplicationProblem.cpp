e7934cdf908302581bcdfcc8222a771ab0ea1b12
lifeChangestartstoday2022 <elbarbori.s.fst@ac.ma>
2022-12-17 21:15:30 +0100

    solution of A multilplication problem

--- amultiplicationProblem.cpp
+++ amultiplicationProblem.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    // at first i used int then since the numbers are big, it overflows so here i am using double
     double A, B, C;
     cin >> A >> B >> C;
     if (A * B >= C)
